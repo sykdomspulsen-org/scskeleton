2da4069e382b4827b97b55fd752545e8deb97b4e
Richard Aubrey White <RichardAubrey.White@fhi.no>
2021-07-14 11:26:00 +0200

    Formatting

--- R/00_env_and_namespace.r
+++ R/00_env_and_namespace.r
@@ -1,4 +1,6 @@
 # ******************************************************************************
+# ******************************************************************************
+#
 # 00_env_and_namespace.r
 #
 # PURPOSE 1:
@@ -6,33 +8,30 @@
 #   because these are the most commonly used packages/functions.
 #
 # PURPOSE 2:
+#   Declaring our own "tm_run_task" inside this package, as a wrapper around
+#   sc::tm_run_task.
+#
+#   We cannot run sc::tm_run_task directly, because we need to load all of the
+#   database connections, db schemas, tasks, etc. *before* we run the task.
+#   Hence, this wrapper ensures that all of this package's configs files are
+#   loaded via OURPACKAGE::.onLoad() first, and then sc::tm_run_task can run.
+#
+# PURPOSE 3:
+#   Declaration of environments that can be used globally.
+#
+# PURPOSE 4:
 #   Fix issues/integration with other packages.
 #
 #   Most notably is the issue with rmarkdown, where an error is thrown when
 #   rendering multiple rmarkdown documents in parallel.
 #
-#   We also need to declare our own "tm_run_task" inside this package, as a
-#   wrapper around sc::tm_run_task. We cannot run sc::tm_run_task directly,
-#   because we need to load all of the database connections, db schemas, tasks,
-#   etc. *before* we run the task. Hence, this wrapper ensures that all of this
-#   package's configs files are loaded via OURPACKAGE::.onLoad() first, and then
-#   sc::tm_run_task can run.
-#
-# PURPOSE 3:
-#   Declaration of environments that can be used globally.
-#
+# ******************************************************************************
 # ******************************************************************************
 
 #' @import ggplot2
 #' @import data.table
 #' @importFrom magrittr %>% %<>%
 1
-
-# https://github.com/rstudio/rmarkdown/issues/1632
-# An error is thrown when rendering multiple rmarkdown documents in parallel.
-clean_tmpfiles_mod <- function() {
-  # message("Calling clean_tmpfiles_mod()")
-}
 
 #' Shortcut to run task
 #'
@@ -58,3 +57,10 @@ tm_run_task <- function(task_name, index_plan = NULL, index_analysis = NULL) {
 #' Declaration of environments that can be used globally
 #' @export config
 config <- new.env()
+
+
+# https://github.com/rstudio/rmarkdown/issues/1632
+# An error is thrown when rendering multiple rmarkdown documents in parallel.
+clean_tmpfiles_mod <- function() {
+  # message("Calling clean_tmpfiles_mod()")
+}
